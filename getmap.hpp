372355f933bbb402f4d69fb3f2bbe73c1129146f
jeong-prj <63942953+jeong-prj@users.noreply.github.com>
2022-06-14 10:47:54 +0900

    Add files via upload

---
+++ getmap.hpp
@@ -0,0 +1,35 @@
+#ifndef INCLUDE_GETMAP_HPP_
+#define INCLUDE_GETMAP_HPP_
+
+#include <ros/ros.h>
+#include <ros/console.h>
+
+#include "nav_msgs/OccupancyGrid.h"
+#include "map_msgs/OccupancyGridUpdate.h"
+//#include "geometry_msgs/PointStamped.h"
+//#include "std_msgs/Header.h"
+//#include "std_msgs/Bool.h"
+#include "nav_msgs/MapMetaData.h"
+//#include "geometry_msgs/Point.h"
+
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/action_client.h>
+
+//#include <tf/transform_listener.h>
+
+
+/*
+protected:
+    //ros::NodeHandle m_nh;
+    //ros::Subscriber m_gridmapsub;
+    int m_mapAvailable;
+    
+    nav_msgs::OccupancyGrid m_gridmap;
+    vector<signed char> m_data;
+    int m_resolution;
+    int m_rows;
+    int m_cols;
+*/    
+
+
+#endif /* INCLUDE_GETMAP_HPP_ */
